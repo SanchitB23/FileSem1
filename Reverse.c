e1e1c1942a65a35ed9dc5a94ad9ef927d7cdb21d
Sanchit Bhatnagar <sanchitbhatnagar4@gmail.com>
2016-12-09 20:42:04 +0530

    Add files via upload

--- Reverse.c
+++ Reverse.c
@@ -2,16 +2,18 @@
 //Incomplete
 #include<stdio.h>
 void main(){
-    int num, rev[10] = 0, i = 1 , j, c = 0;
+    int num, rev[10], i, j, c;
     printf("Enter A Number");
     scanf("%d",&num);
+    c = 0;
+    i = 0;
     while(num != 0){
         rev[i] = num % 10 ;
         num = num / 10 ;
         c++;
         i++;
     }printf("\n");
-    for(j = c ; j > 0 ; j--){
-        printf("%d",rev[j]);
+    for(i = 0 ; i < c ; i++){
+        printf("%d",rev[i]);
     }
 }
