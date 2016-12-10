732d5fe77958c9ddb3b79bd96559ee1d6cd380fe
Sanchit Bhatnagar <sanchitbhatnagar4@gmail.com>
2016-12-10 14:26:06 +0530

    Roots Of Quadratic Equations
    Corrected

--- rootsOfQuadEq.c
+++ rootsOfQuadEq.c
@@ -1,14 +1,21 @@
 // Finding Roots of A Quadratic Equation
 #include<stdio.h>
+#include<math.h>
 void main(){
     float a,b,c,D,x1,x2;
     printf("Enter the value of a,b,c");
     scanf("%f%f%f",&a,&b,&c);
     D=b*b-4*a*c;
-    if(D>0);{
+    if(D>0){
             x1=(-b+sqrt(D))/2*a;
             x2=(-b-sqrt(D))/2*a;
-            printf("\nthe value of x1=%f",x1);
-            printf("\nthe value of x2=%f",x2);
+            printf("\nThe value of Roots are = %f , %f",x1,x2);
+         }
+         else if(D = 0){
+            x1=(-b)/(2*a);
+            printf("\nRoots are Equal = %f",x1);
+         }
+         else {
+            printf("\nNo Real Roots");
          }
  }
